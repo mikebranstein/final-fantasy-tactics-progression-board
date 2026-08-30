821b84c10df7f1480539617f6246dd77270db85d
Mike Branstein <mikeismike@gmail.com>
2026-08-30 12:25:30 +1200

    add: Dragoon (Lv.2 gate) ability load-out for Ramza

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service worker: makes the board work offline once installed.
  * Bump CACHE when you change any cached asset to force an update.
  */
-const CACHE = "fft-board-v6";
+const CACHE = "fft-board-v7";
 const ASSETS = [
   "./",
   "index.html",
